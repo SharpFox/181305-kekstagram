ae0dc2b0a9acc0173873edfd39eda1845504404b
Roman Vishnevsky <roman.bb.vishnevsky@gmail.com>
2017-04-13 03:41:56 +1000

    13.04.2017 #9 Личный проект: милый DOM попытка #3

--- js/pictures.js
+++ js/pictures.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var PHOTO_COMMENTS = [
+var ARRAY_OF_PHOTOS_COMMENTS = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце-концов это просто непрофессионально.',
@@ -8,16 +8,18 @@ var PHOTO_COMMENTS = [
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как-будто их избивают. Как можно было поймать такой неудачный момент?!'];
 
+var PHOTOS_COUNT = 25;
+
 /**
  * Возвращает случайное значение согласно заданному диапазону и округлению.
  *
- * @param {number} lowerLimit
- * @param {number} upperLimit
- * @param {number} roundingRatio
+ * @param {number} min
+ * @param {number} max
  * @return {number}
  */
-var getRandomNumber = function (lowerLimit, upperLimit, roundingRatio) {
-  var randomNumber = Math.random() * (upperLimit - lowerLimit) + lowerLimit;
+var getRandomNumber = function (min, max) {
+  var roundingRatio = 0;
+  var randomNumber = Math.random() * (max - min) + min;
   return parseFloat(randomNumber.toFixed(roundingRatio));
 };
 
@@ -26,15 +28,20 @@ var getRandomNumber = function (lowerLimit, upperLimit, roundingRatio) {
  *
  * @param {string} path
  * @param {string} extension
- * @param {number} photosCount
  * @return {array}
  */
-var getArrayOfPhotosUrls = function (path, extension, photosCount) {
+var getArrayOfPhotosUrls = function (path, extension) {
 
   var arrayOfPhotosUrls = [];
+  var photoUrl;
 
-  for (var i = 1; i < photosCount + 1; i++) {
-    arrayOfPhotosUrls.push(path + '/' + i + '.' + extension);
+  for (var i = 1; i < PHOTOS_COUNT + 1; i++) {
+
+    do {
+      photoUrl = path + '/' + getRandomNumber(1, PHOTOS_COUNT) + '.' + extension;
+    } while (arrayOfPhotosUrls.indexOf(photoUrl) !== -1);
+
+    arrayOfPhotosUrls.push(photoUrl);
   }
   return arrayOfPhotosUrls;
 };
@@ -42,18 +49,16 @@ var getArrayOfPhotosUrls = function (path, extension, photosCount) {
 /**
  * Возвращает массив, содержащий случайное кол-во лайков для каждой фотографии.
  *
- * @param {number} lowerLimitOfRandomNumber
- * @param {number} upperLimitOfRandomNumber
- * @param {number} photosCount
+ * @param {number} minCountLikes
+ * @param {number} maxCountLikes
  * @return {array}
  */
-var getArrayOfPhotosLikes = function (lowerLimitOfRandomNumber, upperLimitOfRandomNumber, photosCount) {
+var getArrayOfPhotosLikes = function (minCountLikes, maxCountLikes) {
 
   var arrayOfPhotosLikes = [];
-  var roundingRatio = 0;
 
-  for (var i = 0; i < photosCount; i++) {
-    arrayOfPhotosLikes.push(getRandomNumber(lowerLimitOfRandomNumber, upperLimitOfRandomNumber, roundingRatio));
+  for (var i = 0; i < PHOTOS_COUNT; i++) {
+    arrayOfPhotosLikes.push(getRandomNumber(minCountLikes, maxCountLikes));
   }
 
   return arrayOfPhotosLikes;
@@ -62,32 +67,31 @@ var getArrayOfPhotosLikes = function (lowerLimitOfRandomNumber, upperLimitOfRand
 /**
  * Возвращает массив, содержащий случайное кол-во комментариев для каждой фотографии.
  *
- * @param {array} arrayOfConstantsPhotosComments
- * @param {number} photosCount
  * @return {array}
  */
-var getArrayOfPhotosComments = function (arrayOfConstantsPhotosComments, photosCount) {
+var getArrayOfPhotosComments = function () {
 
   var arrayOfPhotosComments = [];
-  var roundingRatio = 0;
-  var lowerLimitOfRandomNumber = 1;
-  var upperLimitOfRandomNumber = arrayOfConstantsPhotosComments.length;
 
-  for (var i = 0; i < photosCount; i++) {
+  for (var i = 0; i < PHOTOS_COUNT; i++) {
 
     var commentsCount = getRandomNumber(1, 2);
-    var firstCommentItem = getRandomNumber(lowerLimitOfRandomNumber, upperLimitOfRandomNumber, roundingRatio);
-    var commentText = arrayOfConstantsPhotosComments[firstCommentItem - 1];
+    var firstCommentItem = getRandomNumber(1, ARRAY_OF_PHOTOS_COMMENTS.length);
+    var commentText = ARRAY_OF_PHOTOS_COMMENTS[firstCommentItem - 1];
+    var secondCommentItem;
 
     if (commentsCount === 2) {
 
       do {
-        var secondCommentItem = getRandomNumber(lowerLimitOfRandomNumber, upperLimitOfRandomNumber);
+        secondCommentItem = getRandomNumber(1, ARRAY_OF_PHOTOS_COMMENTS.length);
       } while (secondCommentItem === firstCommentItem);
 
-      commentText += '\n' + arrayOfConstantsPhotosComments[secondCommentItem - 1];
+      commentText += ARRAY_OF_PHOTOS_COMMENTS[secondCommentItem - 1];
     }
-    arrayOfPhotosComments.push(commentText);
+
+    arrayOfPhotosComments[i] = [];
+    arrayOfPhotosComments[i][0] = commentsCount;
+    arrayOfPhotosComments[i][1] = commentText;
   }
 
   return arrayOfPhotosComments;
@@ -117,16 +121,15 @@ var getPhoto = function (url, likesCount, comment) {
  * Возвращает массив объектов, каждый из которого включает в себя
  * url фотографии, кол-во лайков и текст комментария.
  *
- * @param {number} photosCount
  * @param {array} arrayOfPhotosUrls
  * @param {array} arrayOfPhotosLikes
  * @param {array} arrayOfPhotosComments
  * @return {array}
  */
-var getArrayOfPhotos = function (photosCount, arrayOfPhotosUrls, arrayOfPhotosLikes, arrayOfPhotosComments) {
+var getArrayOfPhotos = function (arrayOfPhotosUrls, arrayOfPhotosLikes, arrayOfPhotosComments) {
 
   var arrayOfPhotos = [];
-  for (var i = 0; i < photosCount; i++) {
+  for (var i = 0; i < PHOTOS_COUNT; i++) {
     arrayOfPhotos.push(getPhoto(arrayOfPhotosUrls[i], arrayOfPhotosLikes[i], arrayOfPhotosComments[i]));
   }
 
@@ -140,6 +143,10 @@ var getArrayOfPhotos = function (photosCount, arrayOfPhotosUrls, arrayOfPhotosLi
  */
 var createPhotos = function (arrayOfPhotos) {
 
+  if (arrayOfPhotos.length === 0) {
+    return;
+  }
+
   var similarPictureTemplate = document.querySelector('#picture-template').content;
   var similarListElement = document.querySelector('.pictures');
 
@@ -149,9 +156,9 @@ var createPhotos = function (arrayOfPhotos) {
 
     var pictureTemplateItem = similarPictureTemplate.cloneNode(true);
 
-    pictureTemplateItem.querySelector('img').setAttribute('src', arrayOfPhotos[i].url);
+    pictureTemplateItem.querySelector('img').src = arrayOfPhotos[i].url;
     pictureTemplateItem.querySelector('.picture-likes').textContent = arrayOfPhotos[i].likes;
-    pictureTemplateItem.querySelector('.picture-comments').textContent = arrayOfPhotos[i].comments;
+    pictureTemplateItem.querySelector('.picture-comments').textContent = arrayOfPhotos[i].comments[0];
 
     newPhoto.appendChild(pictureTemplateItem);
   }
@@ -163,37 +170,37 @@ var createPhotos = function (arrayOfPhotos) {
  * Заполняет элемент свойствами.
  *
  * @param {array} arrayOfPhotos
+ * @param {object} galleryOverlayItem
  */
-var fillPhoto = function (arrayOfPhotos) {
-
-  var galleryOverlayItem = document.body.querySelector('.gallery-overlay');
+var fillPhoto = function (arrayOfPhotos, galleryOverlayItem) {
 
   if (arrayOfPhotos.length === 0) {
     return;
   }
 
-  galleryOverlayItem.querySelector('img').setAttribute('src', arrayOfPhotos[0].url);
+  galleryOverlayItem.querySelector('img').src = arrayOfPhotos[0].url;
   galleryOverlayItem.querySelector('.likes-count').textContent = arrayOfPhotos[0].likes;
-  galleryOverlayItem.querySelector('.comments-count').textContent = arrayOfPhotos[0].comments;
+  galleryOverlayItem.querySelector('.comments-count').textContent = arrayOfPhotos[0].comments[0];
 };
 
 var path = 'photos';
 var extension = 'jpg';
-var photosCount = 25;
 
-var arrayOfPhotosUrls = getArrayOfPhotosUrls(path, extension, photosCount);
+var arrayOfPhotosUrls = getArrayOfPhotosUrls(path, extension);
 
-var lowerLimitOfRandomNumber = 15;
-var upperLimitOfRandomNumber = 200;
+var minCountLikes = 15;
+var maxCountLikes = 200;
 
-var arrayOfPhotosLikes = getArrayOfPhotosLikes(lowerLimitOfRandomNumber, upperLimitOfRandomNumber, photosCount);
-var arrayOfPhotosComments = getArrayOfPhotosComments(PHOTO_COMMENTS, photosCount);
+var arrayOfPhotosLikes = getArrayOfPhotosLikes(minCountLikes, maxCountLikes);
+var arrayOfPhotosComments = getArrayOfPhotosComments();
 
-var arrayOfPhotos = getArrayOfPhotos(photosCount, arrayOfPhotosUrls, arrayOfPhotosLikes, arrayOfPhotosComments);
+var arrayOfPhotos = getArrayOfPhotos(arrayOfPhotosUrls, arrayOfPhotosLikes, arrayOfPhotosComments);
 
 createPhotos(arrayOfPhotos);
 
-document.body.querySelector('.upload-overlay').classList.add('invisible');
-document.body.querySelector('.gallery-overlay').classList.remove('invisible');
+document.querySelector('.upload-overlay').classList.add('invisible');
 
-fillPhoto(arrayOfPhotos);
+var galleryOverlayItem = document.querySelector('.gallery-overlay');
+galleryOverlayItem.classList.remove('invisible');
+
+fillPhoto(arrayOfPhotos, galleryOverlayItem);
